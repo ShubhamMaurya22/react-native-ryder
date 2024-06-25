4347ebf08c61607977cf2a85444901159ca7c7fc
ShubhamMaurya22 <shubhammaurya8698@gmail.com>
2024-06-25 12:52:33 +0530

    files added

--- firebase.js
+++ firebase.js
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth,initializeAuth, getReactNativePersistence} from "firebase/auth";
@@ -8,13 +9,13 @@ import firebase from 'firebase/app';
 import 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyBQD6iAK51PywoAGQPVI8xJf3Tuj394psw",
+  authDomain: "ryder-401215.firebaseapp.com",
+  projectId: "ryder-401215",
+  storageBucket: "ryder-401215.appspot.com",
+  messagingSenderId: "716770071320",
+  appId: "1:716770071320:web:992416139f6e1d4341c46d",
+  measurementId: "G-7SVQLLNN05"
 };
 
 // Initialize Firebase
@@ -29,4 +30,8 @@ export {app, auth, db}
 
 
 
+// export const API_KEY = '0b3a6a5d-c3a0-4f21-9e53-6693ce03a1e9';
 
+
+// export const client_id = 'free_tier_maurya.shubham_485200217e'
+// export const client_secret = 'fb4e849e933e4153859a9ae18b4d238b'
